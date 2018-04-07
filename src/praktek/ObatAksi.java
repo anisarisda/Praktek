00752de4bba2a93cf5d96b842a832c632023520f
anisa <37506225+anisaicananda@users.noreply.github.com>
2018-04-07 10:27:27 +0700

    Memanggil Method

--- src/praktek/ObatAksi.java
+++ src/praktek/ObatAksi.java
@@ -14,32 +14,32 @@ public class ObatAksi {
         Obat cair = new Obat();
         Obat tablet = new Obat();
         Obat serbuk = new Obat();
-        Obat Behavior = new Obat();
         
         cair.merk="Hufagrip";
         cair.jenis="Zat Cair";
         cair.isi="125 ml";
         cair.warna="Merah";
         cair.kemasan="Botol Kaca";
+        cair.Behavior="Mencegah, mengobati dan menyembuhkan";
         
         tablet.merk="Combantrine";
         tablet.jenis="Zat Padat";
         tablet.isi="500 mg";
         tablet.warna="Orange";
         tablet.kemasan="Plastik";
+        tablet.Behavior="Mencegah, mengobati dan menyembuhkan";
         
         serbuk.merk="Adam Sari";
         serbuk.jenis="Zat Padat kecil";
         serbuk.isi="2,5 gr";
         serbuk.warna="Putih";
         serbuk.kemasan="Alumunium Foil";
-        
-        Behavior.Behavior="Mencegah, mengobati dan menyembuhkan";
+        serbuk.Behavior="Mencegah, mengobati dan menyembuhkan";
         
         cair.cetakInfo();
         tablet.cetakInfo();
         serbuk.cetakInfo();
-        Behavior.cetakInfo();
+        
     
 }
 }
