5902c9721582266c11f17d1e10eac426fb5e2686
anisa <37506225+anisaicananda@users.noreply.github.com>
2018-04-07 10:11:51 +0700

    Mengisi Atribut

--- src/praktek/ObatAksi.java
+++ src/praktek/ObatAksi.java
@@ -15,6 +15,25 @@ public class ObatAksi {
         Obat tablet = new Obat();
         Obat serbuk = new Obat();
         
+        cair.merk="Hufagrip";
+        cair.jenis="Zat Cair";
+        cair.isi="125 ml";
+        cair.warna="Merah";
+        cair.kemasan="Botol Kaca";
+        
+        tablet.merk="Combantrine";
+        tablet.jenis="Zat Padat";
+        tablet.isi="500 mg";
+        tablet.warna="Orange";
+        tablet.kemasan="Plastik";
+        
+        serbuk.merk="Adam Sari";
+        serbuk.jenis="Zat Padat kecil";
+        serbuk.isi="2,5 gr";
+        serbuk.warna="Putih";
+        serbuk.kemasan="Alumunium Foil";
+        
+        
     
 }
 }
