b8fa57211e6b7ad6cb1814f147d3802f8984a183
anisa <37506225+anisaicananda@users.noreply.github.com>
2018-04-07 09:56:30 +0700

    Class Aksi dan Method main

---
+++ src/praktek/ObatAksi.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek;
+
+/**
+ *
+ * @author win 10
+ */
+public class ObatAksi {
+    public static void main (String[] args) {
+}
+}
